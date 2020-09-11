d18b54646e7e3881209dcaeea6d0809d786e3a2e
berkaycelik <bcelik@rapsodo.com>
2020-09-12 01:03:20 +0300

    create basic node app

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const APP_PORT = process.env.APP_PORT || 8080;
+
+app.listen(APP_PORT, () => {
+     console.log(`App is running on ${APP_PORT}`);
+});
