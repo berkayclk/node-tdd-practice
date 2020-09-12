615e089cd270d8cbffce3e0f99568ddf8c53bc91
berkaycelik <bcelik@rapsodo.com>
2020-09-12 20:17:16 +0300

    sync sequelize before start the server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const dotenv = require('dotenv');
 dotenv.config(); // load .env file
 
+const sequelize = require('./src/config/database');
+sequelize.sync();
+
 const app = require('./src/app');
 const { APP_PORT } = require('./src/constants/appConfig');
 
