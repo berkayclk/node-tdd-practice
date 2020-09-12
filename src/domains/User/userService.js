457f728e212073cc196d984bf84c86295013c73d
berkaycelik <bcelik@rapsodo.com>
2020-09-12 20:36:12 +0300

    hash password while creating user

--- src/domains/User/userService.js
+++ src/domains/User/userService.js
@@ -1,8 +1,12 @@
 const User = require('./User');
+const bcrypt = require('bcrypt');
 
 exports.saveUser = async (userToCreate) => {
   if (!userToCreate) return null;
 
+  const hashedPassword = bcrypt.hashSync(userToCreate.password, 10);
+
+  userToCreate = { ...userToCreate, password: hashedPassword };
   const savedUser = await User.create(userToCreate);
   delete savedUser.password;
 
