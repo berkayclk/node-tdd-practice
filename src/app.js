e907ceb68c09c9e507d9cad43576899efda19853
berkaycelik <bcelik@rapsodo.com>
2020-09-12 19:58:49 +0300

    use bodyParser instead of express.json

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 app.use(express.json());
 
 module.exports = app;
-
