7c2a443f30941e804952b7b6148ae82a4a5cd230
berkaycelik <bcelik@rapsodo.com>
2020-09-12 16:35:36 +0300

    add lint packages to project

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express();
 const APP_PORT = process.env.APP_PORT || 8080;
 
 app.listen(APP_PORT, () => {
-     console.log(`App is running on ${APP_PORT}`);
+  console.log(`App is running on ${APP_PORT}`);
 });
