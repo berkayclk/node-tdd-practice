2375f2f37a6a08ea46ec83a69496049e93528c52
berkaycelik <bcelik@rapsodo.com>
2020-09-12 17:17:01 +0300

    create user model

---
+++ src/User/User.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const Model = Sequelize.Model;
+
+class User extends Model {}
+
+User.init(
+  {
+    username: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
